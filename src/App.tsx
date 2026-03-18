269a5239056a9fb398feadffb8235ab2d24b298b
Noctua76 <iliaskalivas@hotmail.com>
2026-03-18 23:21:28 +0200

    Fix indentation in App.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter basename="/aegis-link-landing-page/">
-        <Routes>
+  <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
