133177b0f9246dcd9b9b7a50fb8c4e35d96ec965
Noctua76 <iliaskalivas@hotmail.com>
2026-03-19 11:00:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <HashRouter>
   <Routes>
     <Route path="/" element={<Index />} />
-    <Route path="*" element={<NotFound />} />
+    <Route path="*" element={<Index />} />
   </Routes>
 </HashRouter>
     </TooltipProvider>
